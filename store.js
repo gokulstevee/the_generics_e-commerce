e31fddb574c42aa9643013d3aa58a6ff07fef56c
gokulstevee <65470058+gokulstevee@users.noreply.github.com>
2020-08-30 01:17:25 +0530

    update_card

--- store.js
+++ store.js
@@ -16,4 +16,19 @@ function ready() {
   }
 }
 
-
+function updateCardTotal() {
+  var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      "cart-quantity-input"
+    )[0];
+    var price = parseFloat(priceElement.innerHTML.replace("$", ""));
+    var quantity = quantityElement.value;
+    var total = price * quantity;
+  }
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$" + total;
+}
