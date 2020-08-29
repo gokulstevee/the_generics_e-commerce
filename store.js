6a1b5401be61c34f52b5169229cab0ae22cf8058
gokulstevee <65470058+gokulstevee@users.noreply.github.com>
2020-08-30 01:16:07 +0530

    remove_cart

---
+++ store.js
@@ -0,0 +1,19 @@
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  var removeCartItemButtons = document.getElementsByClassName("btn-danger");
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener("click", (event) => {
+      var buttonClicked = event.target;
+      buttonClicked.parentElement.parentElement.remove();
+      updateCardTotal();
+    });
+  }
+}
+
+
